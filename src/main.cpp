786bed172b2bc2430f56b81dbd253f26def12c7d
Erymanthus (RayDeeUx) <51521765+RayDeeUx@users.noreply.github.com>
2025-06-07 21:20:25 -0400

    im gonna break something i swear to fod

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,12 @@ class $modify(MyPlayLayer, PlayLayer) {
 	void resetLevel() {
 		PlayLayer::resetLevel();
 		if (!Mod::get()->getSettingValue<bool>("enabled") || !m_isTestMode || !m_startPosObject || !m_startPosObject->m_startSettings) return;
-		static_cast<GJBaseGameLayer*>(this)->toggleFlipped(false, m_startPosObject->m_startSettings->m_mirrorMode);
+		static_cast<GJBaseGameLayer*>(this)->toggleFlipped(m_startPosObject->m_startSettings->m_mirrorMode, false);
 	}
 	void setupHasCompleted() {
 		PlayLayer::setupHasCompleted();
 		if (!Mod::get()->getSettingValue<bool>("enabled") || !m_isTestMode || !m_startPosObject || !m_startPosObject->m_startSettings) return;
-		static_cast<GJBaseGameLayer*>(this)->toggleFlipped(false, m_startPosObject->m_startSettings->m_mirrorMode);
+		static_cast<GJBaseGameLayer*>(this)->toggleFlipped(m_startPosObject->m_startSettings->m_mirrorMode, false);
 	}
 };
 
