2206018e68de0f63be0e301214b786f7753601dd
Erymanthus (RayDeeUx) <51521765+RayDeeUx@users.noreply.github.com>
2025-06-07 20:43:13 -0400

    i probably broke the whole damn thing

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
-#include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/PlayLayer.hpp>
 
 using namespace geode::prelude;
 
-class $modify(MyMenuLayer, MenuLayer) {
-	bool init() {
-		if (!MenuLayer::init()) return false;
-
-		return true;
+class $modify(MyPlayLayer, PlayLayer) {
+	void resetLevel() {
+		PlayLayer::resetLevel();
+		if (!Mod::get()->getSettingValue<bool>("enabled") || !m_isTestMode || !m_startPosObject || !m_startPosObject->m_startSettings) return;
+		PlayLayer::toggleFlipped(m_startPosObject->m_startSettings->m_isFlipped, false);
+		log::info("[resetLevel] flipped");
+	}
+	void setupHasCompleted() {
+		PlayLayer::setupHasCompleted();
+		if (!Mod::get()->getSettingValue<bool>("enabled") || !m_isTestMode || !m_startPosObject || !m_startPosObject->m_startSettings) return;
+		PlayLayer::toggleFlipped(m_startPosObject->m_startSettings->m_isFlipped, false);
+		log::info("[setupHasCompleted] flipped");
 	}
 };
