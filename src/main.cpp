6b2ae6bf8a919cb35071abb485fa6d486797dbef
Erymanthus (RayDeeUx) <51521765+RayDeeUx@users.noreply.github.com>
2025-06-07 21:16:49 -0400

    sdfjkrejwiufnjkr

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <Geode/modify/GJBaseGameLayer.hpp>
 #include <Geode/modify/PlayLayer.hpp>
 
 using namespace geode::prelude;
@@ -6,16 +7,19 @@ class $modify(MyPlayLayer, PlayLayer) {
 	void resetLevel() {
 		PlayLayer::resetLevel();
 		if (!Mod::get()->getSettingValue<bool>("enabled") || !m_isTestMode || !m_startPosObject || !m_startPosObject->m_startSettings) return;
-		PlayLayer::toggleFlipped(false, m_startPosObject->m_startSettings->m_mirrorMode);
+		static_cast<GJBaseGameLayer*>(this)->toggleFlipped(false, m_startPosObject->m_startSettings->m_mirrorMode);
 	}
 	void setupHasCompleted() {
 		PlayLayer::setupHasCompleted();
 		if (!Mod::get()->getSettingValue<bool>("enabled") || !m_isTestMode || !m_startPosObject || !m_startPosObject->m_startSettings) return;
-		PlayLayer::toggleFlipped(false, m_startPosObject->m_startSettings->m_mirrorMode);
+		static_cast<GJBaseGameLayer*>(this)->toggleFlipped(false, m_startPosObject->m_startSettings->m_mirrorMode);
 	}
+};
+
+class $modify(MyGJBaseGameLayer, GJBaseGameLayer) {
 	void toggleFlipped(bool whatTheFUCK, bool fuckingHelpME) {
 		log::info("whatTheFUCK (false): {}", whatTheFUCK);
 		log::info("fuckingHelpME (m_startPosObject->m_startSettings->m_mirrorMode): {}", fuckingHelpME);
-		PlayLayer::toggleFlipped(whatTheFUCK, fuckingHelpME);
+		GJBaseGameLayer::toggleFlipped(whatTheFUCK, fuckingHelpME);
 	}
 };
