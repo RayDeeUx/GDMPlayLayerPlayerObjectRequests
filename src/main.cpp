f4f44d1bf87540acf5312443344f0fbaab879c5a
Erymanthus (RayDeeUx) <51521765+RayDeeUx@users.noreply.github.com>
2025-06-07 21:58:18 -0400

    optimize?

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <Geode/modify/GJBaseGameLayer.hpp>
 #include <Geode/modify/PlayLayer.hpp>
 
 using namespace geode::prelude;
@@ -7,13 +6,6 @@ class $modify(MyPlayLayer, PlayLayer) {
 	void resetLevel() {
 		PlayLayer::resetLevel();
 		if (!Mod::get()->getSettingValue<bool>("enabled") || !m_isTestMode || !m_startPosObject || !m_startPosObject->m_startSettings) return;
-		log::info("[resetLevel] m_startPosObject->m_startSettings->m_mirrorMode: {}", m_startPosObject->m_startSettings->m_mirrorMode);
-		static_cast<GJBaseGameLayer*>(this)->toggleFlipped(m_startPosObject->m_startSettings->m_mirrorMode, true);
-	}
-	void setupHasCompleted() {
-		PlayLayer::setupHasCompleted();
-		if (!Mod::get()->getSettingValue<bool>("enabled") || !m_isTestMode || !m_startPosObject || !m_startPosObject->m_startSettings) return;
-		log::info("[setupHasCompleted] m_startPosObject->m_startSettings->m_mirrorMode: {}", m_startPosObject->m_startSettings->m_mirrorMode);
-		static_cast<GJBaseGameLayer*>(this)->toggleFlipped(m_startPosObject->m_startSettings->m_mirrorMode, true);
+		PlayLayer::toggleFlipped(m_startPosObject->m_startSettings->m_mirrorMode, true);
 	}
 };
