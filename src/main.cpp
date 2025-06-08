8ea52de5eec4461ccffa6907477f1276b76cf984
Erymanthus (RayDeeUx) <51521765+RayDeeUx@users.noreply.github.com>
2025-06-07 21:11:48 -0400

    FEIDMKLEMDGIEFIOEJDIOEDOIE

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,16 @@ class $modify(MyPlayLayer, PlayLayer) {
 	void resetLevel() {
 		PlayLayer::resetLevel();
 		if (!Mod::get()->getSettingValue<bool>("enabled") || !m_isTestMode || !m_startPosObject || !m_startPosObject->m_startSettings) return;
-		this->toggleFlipped(false, m_startPosObject->m_startSettings->m_mirrorMode);
+		PlayLayer::toggleFlipped(false, m_startPosObject->m_startSettings->m_mirrorMode);
 	}
 	void setupHasCompleted() {
 		PlayLayer::setupHasCompleted();
 		if (!Mod::get()->getSettingValue<bool>("enabled") || !m_isTestMode || !m_startPosObject || !m_startPosObject->m_startSettings) return;
-		this->toggleFlipped(false, m_startPosObject->m_startSettings->m_mirrorMode);
+		PlayLayer::toggleFlipped(false, m_startPosObject->m_startSettings->m_mirrorMode);
+	}
+	void toggleFlipped(bool whatTheFUCK, bool fuckingHelpME) {
+		log::info("whatTheFUCK (false): {}", whatTheFUCK);
+		log::info("fuckingHelpME (m_startPosObject->m_startSettings->m_mirrorMode): {}", fuckingHelpME);
+		PlayLayer::toggleFlipped(whatTheFUCK, fuckingHelpME);
 	}
 };
