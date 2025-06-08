550a5f94dd844fbc38f60f66fe9b6d81eb6c5ce8
Erymanthus (RayDeeUx) <51521765+RayDeeUx@users.noreply.github.com>
2025-06-07 20:57:33 -0400

    im fucking stupid

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,13 @@ class $modify(MyPlayLayer, PlayLayer) {
 	void resetLevel() {
 		PlayLayer::resetLevel();
 		if (!Mod::get()->getSettingValue<bool>("enabled") || !m_isTestMode || !m_startPosObject || !m_startPosObject->m_startSettings) return;
-		PlayLayer::toggleFlipped(m_startPosObject->m_startSettings->m_isFlipped, false);
+		PlayLayer::toggleFlipped(m_startPosObject->m_startSettings->m_mirrorMode, false);
 		log::info("[resetLevel] flipped");
 	}
 	void setupHasCompleted() {
 		PlayLayer::setupHasCompleted();
 		if (!Mod::get()->getSettingValue<bool>("enabled") || !m_isTestMode || !m_startPosObject || !m_startPosObject->m_startSettings) return;
-		PlayLayer::toggleFlipped(m_startPosObject->m_startSettings->m_isFlipped, false);
+		PlayLayer::toggleFlipped(m_startPosObject->m_startSettings->m_mirrorMode, false);
 		log::info("[setupHasCompleted] flipped");
 	}
 };
