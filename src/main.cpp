d54794b352b64e7d352118c54c8e255be7cc734c
Erymanthus (RayDeeUx) <51521765+RayDeeUx@users.noreply.github.com>
2025-06-07 22:09:47 -0400

    aaaaaAAAAAAAAAAA

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,6 @@ class $modify(MyPlayLayer, PlayLayer) {
 	void resetLevel() {
 		PlayLayer::resetLevel();
 		if (!Mod::get()->getSettingValue<bool>("enabled") || !m_isTestMode || !m_startPosObject || !m_startPosObject->m_startSettings) return;
-		static_cast<GJBaseGameLayer*>(this)->toggleFlipped(m_startPosObject->m_startSettings->m_mirrorMode, true);
-	}
-	void setupHasCompleted() {
-		PlayLayer::setupHasCompleted();
-		if (!Mod::get()->getSettingValue<bool>("enabled") || !m_isTestMode || !m_startPosObject || !m_startPosObject->m_startSettings) return;
-		static_cast<GJBaseGameLayer*>(this)->toggleFlipped(m_startPosObject->m_startSettings->m_mirrorMode, true);
+		PlayLayer::toggleFlipped(m_startPosObject->m_startSettings->m_mirrorMode, true);
 	}
 };
