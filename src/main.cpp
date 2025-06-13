e354aa7ef4cf1bebc01ff338d4fceb1685ce7caf
Erymanthus (RayDeeUx) <51521765+RayDeeUx@users.noreply.github.com>
2025-06-12 22:46:59 -0400

    remove log

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@ using namespace geode::prelude;
 class $modify(MyPlayerObject, PlayerObject) {
 	void togglePlayerScale(bool p0, bool p1) {
 		PlayerObject::togglePlayerScale(p0, p1);
-		log::info("changing size");
 		GJBaseGameLayer* gjbgl = GJBaseGameLayer::get();
 		if (!gjbgl || (this != gjbgl->m_player1 && this != gjbgl->m_player2) || !this->m_isSwing) return;
 		PlayerObject::updatePlayerSwingFrame(GameManager::get()->getPlayerSwing());
