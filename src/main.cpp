62f9859bb43afdf09251436a8d943d95bc6a1ea3
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2025-06-17 08:48:25 -0400

    oh what the hell

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ using namespace geode::prelude;
 class $modify(MyPlayerObject, PlayerObject) {
 	void update(float dt) {
 		PlayerObject::update(dt);
-		if (!m_gameLayer || !this->isInNormalMode() || (this != m_gameLayer->m_player1 && this != m_gameLayer->m_player2) || m_isOnGround) return;
+		if (!m_gameLayer || !this->isInNormalMode() || (this != m_gameLayer->m_player1 && this != m_gameLayer->m_player2) || m_isOnGround || m_isOnGround2 || m_isOnGround3 || m_isOnGround4) return;
 		PlayerObject::setRotation(0);
 	}
 };
