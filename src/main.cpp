1575a49cb4279ec6f5794b8e56d50c02ad03e6c9
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2025-06-17 06:54:15 -0400

    hmm.

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,6 @@ class $modify(MyPlayerObject, PlayerObject) {
 	void update(float dt) {
 		PlayerObject::update(dt);
 		if (!m_gameLayer || !this->isInNormalMode() || !this->m_mainLayer || (this != m_gameLayer->m_player1 && this != m_gameLayer->m_player2)) return;
-		if (!m_isOnGround) m_mainLayer->setRotation(0);
+		if (!m_isOnGround) PlayerObject::setRotation(0);
 	}
 };
