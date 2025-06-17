eaa1a15d8180a586b7d9e07f7afc3d2fbfa3463b
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2025-06-17 13:34:07 -0400

    OK TEST DONE

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,4 @@ class $modify(MyPlayerObject, PlayerObject) {
 		if (!m_gameLayer || !this->isInNormalMode() || (this != m_gameLayer->m_player1 && this != m_gameLayer->m_player2) || m_isOnGround || m_isOnSlope) return;
 		PlayerObject::setRotation(0);
 	}
-	void togglePlayerScale(bool p0, bool p1) {
-		PlayerObject::togglePlayerScale(p0, p1);
-		if (!m_isSwing || !m_defaultMiniIcon || !m_gameLayer || (this != m_gameLayer->m_player1 && this != m_gameLayer->m_player2)) return;
-		PlayerObject::updatePlayerSwingFrame(GameManager::get()->getPlayerSwing());
-	}
 };
