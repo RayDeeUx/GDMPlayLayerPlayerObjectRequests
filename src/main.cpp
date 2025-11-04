1beb31e7f5ce73efd5ff03783e5cb1a497212dc4
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2025-11-04 15:55:13 -0500

    .01 again

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,7 @@ class $modify(MyGJItemIcon, GJItemIcon) {
 	bool init(UnlockType p0, int p1, cocos2d::ccColor3B p2, cocos2d::ccColor3B p3, bool p4, bool p5, bool p6, cocos2d::ccColor3B p7) {
 		if (!GJItemIcon::init(p0, p1, p2, p3, p4, p5, p6, p7)) return false;
 		m_fields->unlockType = p0;
-		this->scheduleOnce(schedule_selector(MyGJItemIcon::scaleGracefully), .05f);
+		this->scheduleOnce(schedule_selector(MyGJItemIcon::scaleGracefully), .01f);
 		return true;
 	}
 };
