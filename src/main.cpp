7732295661efc54531443445643196b284516eac
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2025-11-04 15:21:07 -0500

    for fuck sake

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,7 @@
 using namespace geode::prelude;
 
 class $modify(MyGJItemIcon, GJItemIcon) {
-	struct Fields {
-		float futureScale = 1.f;
-	};
 	void scaleGracefully(float dt) {
-		this->setScale(m_fields->futureScale);
-	}
-	bool init(UnlockType p0, int p1, cocos2d::ccColor3B p2, cocos2d::ccColor3B p3, bool p4, bool p5, bool p6, cocos2d::ccColor3B p7) {
-		if (!GJItemIcon::init(p0, p1, p2, p3, p4, p5, p6, p7)) return false;
 		if (!m_player) return true;
 		auto simplePlayerChildSprite = m_player->getChildByType<CCSprite>(0);
 		if (!simplePlayerChildSprite || simplePlayerChildSprite->getChildrenCount() < 4) return true;
@@ -35,10 +28,12 @@ class $modify(MyGJItemIcon, GJItemIcon) {
 		
 		log::info("futureScale: {}", futureScale);
 		log::info("==========================================");
-		if (originalScale != futureScale) {
-			m_fields->futureScale = futureScale;
-			this->scheduleOnce(schedule_selector(MyGJItemIcon::scaleGracefully), .05f);
-		}
+
+		this->setScale(futureScale);
+	}
+	bool init(UnlockType p0, int p1, cocos2d::ccColor3B p2, cocos2d::ccColor3B p3, bool p4, bool p5, bool p6, cocos2d::ccColor3B p7) {
+		if (!GJItemIcon::init(p0, p1, p2, p3, p4, p5, p6, p7)) return false;
+		this->scheduleOnce(schedule_selector(MyGJItemIcon::scaleGracefully), .05f);
 		return true;
 	}
 };
