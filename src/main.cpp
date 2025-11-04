3500904ad1b9279f7f8b5272ce05989e54e7ec9a
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2025-11-04 15:41:07 -0500

    fuck it

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ class $modify(MyGJItemIcon, GJItemIcon) {
 		log::info("futureScale: {}", futureScale);
 		log::info("==========================================");
 
-		this->setScale(futureScale);
+		if (futureScale < originalScale) this->setScale(futureScale);
 	}
 	bool init(UnlockType p0, int p1, cocos2d::ccColor3B p2, cocos2d::ccColor3B p3, bool p4, bool p5, bool p6, cocos2d::ccColor3B p7) {
 		if (!GJItemIcon::init(p0, p1, p2, p3, p4, p5, p6, p7)) return false;
